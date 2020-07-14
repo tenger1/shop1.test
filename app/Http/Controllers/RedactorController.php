96ccb84d553b64c58de958880112efdcba0ab3a7
artadamqwerty <66947043+artadamqwerty@users.noreply.github.com>
2020-07-15 00:52:53 +0300

    Add files via upload

--- app/Http/Controllers/RedactorController.php
+++ app/Http/Controllers/RedactorController.php
@@ -40,20 +40,87 @@ class RedactorController extends Controller
    }
    
    public function SearchByNameAction (){
+        $result = false;
+        
         $name = ($_GET["name_order"]); 
+        $id_from = ($_GET["id_from"]); 
+        $id_to = ($_GET["id_to"]); 
         
         $servername = "127.0.0.1:3308";
         $username = "user1";
         $password = "12345";
         $dbname = "shop";
+       /* 
+        if($name == '' && ($id_from != '' || $id_to != '')){
+            echo "GOOD FROM!";
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            $result = mysqli_query($conn, "SELECT * FROM orders WHERE id>='$id_from' AND id<='$id_to'");
+        }
+        */
+
+        /*
+        if($name != ' ' && ($id_from != '' || $id_to != '')){
+           $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE customer_name='$name' AND order_id BETWEEN '$id_from' AND '$id_to'");
+        echo "if 1";
+        }
+         * 
+         */
         
+        if($id_from == '') echo 123;
+        
+        if($name == '' && $id_from != '' && $id_to != ''){
+           $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE order_id BETWEEN '$id_from' AND '$id_to'");
+        
+        }
+        
+        
+        if($name != '' && $id_from == '' && $id_to == ''){
         $conn = mysqli_connect($servername, $username, $password, $dbname);
-        $result = mysqli_query($conn, "SELECT * FROM orders WHERE customer_name='$name'");
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE adress LIKE '%$name%'");
+
+        }
         
+        
+        
+        if($name != '' && $id_from != '' && $id_to == ''){
+            $id_from = ($_GET["id_from"]); 
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE adress LIKE '%$name%' AND order_id>='$id_from'");
+
+        }
+        if($name != '' && $id_from == '' && $id_to != ''){
+            $id_to = ($_GET["id_to"]); 
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE adress LIKE '%$name%' AND order_id<='$id_to'");
+
+        }
+        if($name != '' && $id_from != '' && $id_to != ''){
+            $id_to = ($_GET["id_to"]); 
+            $id_from = ($_GET["id_from"]); 
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE adress LIKE '%$name%' AND order_id<='$id_to' AND order_id>='$id_from'");
+
+        }
+        
+        if($name == '' && $id_from == '' && $id_to != ''){
+           $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE order_id<='$id_to'");
+        
+
+        }
+        if($name == '' && $id_from != '' && $id_to == ''){
+           $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $result = mysqli_query($conn, "SELECT * FROM orders WHERE order_id>='$id_from'");
+        
+
+        }
+ 
+        if($result){
         for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+        }
         
-
-
 /*
         if($result){
             var_dump($result);
