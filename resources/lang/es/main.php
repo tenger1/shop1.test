3997e350b6e39d295f5fe7bcac759cfb5a245d2c
artadamqwerty <66947043+artadamqwerty@users.noreply.github.com>
2020-07-22 01:09:21 +0300

    Add files via upload

---
+++ resources/lang/es/main.php
@@ -0,0 +1,68 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+return[
+'our_flowers_base' => 'Nuestra base de flores',
+    'home' => 'Inicio',
+    'autor-ziedi' => 'autor-flores',
+    'ieiet' => 'Iniciar sesión',
+    'Reg' => 'Registrador',
+    'Grozs' => 'Carrito',
+    'Hello' => 'Hola',
+    'Redactor' => 'Redactor',
+    'viesis' => 'invitado',
+    'prece' => 'item',
+    'Search' => 'Buscar',
+    'info' => 'Nuestra base “A & A” fue creada en 2020. año.
+                            <br> Sus fundadores son profesionales, floristas <strong> Arthur </strong> y <strong> Artem. </strong> <br>
+                            Habiéndose educado en las mejores universidades del mundo, han decidido complacer a las personas de los alrededores con la belleza de las flores.
+                            <br> <mark> <strong> Somos una floristería y estamos listos para tomar sus pedidos 24/7! </strong> </mark> <br> Cada semana entregamos productos de todo el mundo con las mejores flores. Solo con nosotros puede comprar las flores más hermosas, aromáticas y duraderas que derretirán cualquier corazón y deleitarán sus ojos durante más de una semana. <br> Haga un pedido en línea o vaya a nuestra tienda en 19 Raina Boulevard. <br> Ofrecemos nuestros ramos de flores, los vendemos en piezas y hacemos pedidos de acuerdo a sus deseos. ',
+    'nos' => ' "A & A ”Base de flores ',
+    'Pakalpojums' => 'Servicio',
+    'Cena' => 'Precio',
+    'Lentīte' => 'Arco',
+    'Apsv' => 'Tarjeta de felicitación',
+    'Iesaiņojums' => 'Embalaje',
+    'san' => 'Recibiendo en base',
+    'piegr' => 'Entrega en Riga',
+    'piegarp' => 'Entrega fuera de Riga',
+    'piegarpdl' => 'Entrega fuera de servicio',
+    'atl' => 'Descuento con tarjeta de cliente',
+    'atln' => 'El descuento no regresó a servicios adicionales',
+    'up' => 'Subir',
+    'kur' => '¿Dónde estamos?',
+    'sazin' => '¿Cómo contactarnos?',
+    'address' => 'Raina Boulevard 19, Distrito Central, Riga, LV-1586',
+    'sekot' => 'Síguenos:',
+    
+    
+    'in' => 'Iniciar sesión en la base',
+    'Atp' => 'Atrás',
+    'Kurš' => '¿Quién eres?',
+    'obl' => 'Requared',
+    'nos' => 'Nombre',
+    'Daudz' => 'Pcs.',
+    'jps' => 'Total:',
+    'delid' => 'Eliminar por ID:',
+    'del' => 'Eliminar',
+    'allcart' => 'all cart',
+    'your_name' => 'Tu nombre',
+    'ya' => 'Dirección',
+    'tnum' => 'Teléfono',
+    'papinfo' => 'información adicional',
+    'sanl' => 'Fecha y hora de recepción',
+    'pas' => 'Pedido',
+    'Krāsa' => 'Color',
+    'Valsts' => 'País',
+    'Apraksts' => 'Descripción',
+    'spnp' => 'No tenemos esto',
+    'cd' => '¿Cuánto?',
+    'tocart' => 'En carrito',
+    'in_your_cart' => 'En su carrito',
+    'your_cart' => 'Su carrito',
+    
+    ];
